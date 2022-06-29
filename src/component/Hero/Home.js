9200af1ab85c2579f09c64d0562f7d80be8ed384
Tufoin Nkuo <tufoinnkuo10@gmail.com>
2022-06-29 07:48:36 +0100

    fix

--- src/component/Hero/Home.js
+++ src/component/Hero/Home.js
@@ -36,14 +36,14 @@ const Home = () => {
             </h2>
 
             <p>
-              I can help you build a product , feature or website Look through
+              I can help you build a product, feature or website, Look through
               some of my work and experience! If you like what you see and have
-              a project you need coded, don’t hestiate to contact me..
+              a project you need coded, don’t hestiate to contact me.
             </p>
 
             <div className="hero_btn d_flex">
               <div className="col_1">
-                <h4>FIND WITH ME</h4>
+                <h4>Get To Me Through..</h4>
                 <div className="button">
                   <a href={url2} target="_blank" rel="noopener noreferrer">
                     <button className="btn_shadow">
